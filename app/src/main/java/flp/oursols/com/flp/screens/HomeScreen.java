0c3449f61af821f4c248be61f0aa79d26bee5fbf
Anwar <anwar@hybriditservices.com>
2016-10-02 19:22:23 +0500

    new Updates
    new updates

--- app/src/main/java/flp/oursols/com/flp/screens/HomeScreen.java
+++ app/src/main/java/flp/oursols/com/flp/screens/HomeScreen.java
@@ -1,7 +1,13 @@
 package flp.oursols.com.flp.screens;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +16,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import flp.oursols.com.flp.R;
 
-public class HomeScreen extends FragmentActivity implements OnMapReadyCallback {
+public class HomeScreen extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+    FloatingActionButton fab1;
+    Animation show_fab_1;
+    Animation hide_fab_1;
+    boolean isShow=false;
+    FloatingActionButton FAButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +32,41 @@ public class HomeScreen extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+ 
+        fab1 =(FloatingActionButton) findViewById(R.id.fab_1);
+        FAButton =(FloatingActionButton) findViewById(R.id.mainFab);
+        FAButton.setOnClickListener(this);
+
+         show_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_show);
+         hide_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_hide);
+
+
+       
+
+        displayFab();
+        hideFab();
+
 
     }
 
+    private void hideFab() {
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+        layoutParams.rightMargin -= (int) (fab1.getWidth() * 1.7);
+        layoutParams.bottomMargin -= (int) (fab1.getHeight() * 0.25);
+        fab1.setLayoutParams(layoutParams);
+        fab1.startAnimation(hide_fab_1);
+        fab1.setClickable(false);
+    }
 
-
+    private void displayFab() {
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+        layoutParams.rightMargin -= (int) (fab1.getWidth() * 1.7);
+        layoutParams.bottomMargin -= (int) (fab1.getHeight() * 0.25);
+        fab1.setLayoutParams(layoutParams);
+        fab1.startAnimation(hide_fab_1);
+        fab1.setClickable(false);
+    
+    }
 
 
     @Override
@@ -35,5 +77,21 @@ public class HomeScreen extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title(""));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.mainFab:
+                if(isShow){
+                    hideFab();
+                    isShow=false;
+                }else{
+                    displayFab();
+                    isShow=true;
+                }
+                break;
+        }
     }
 }
